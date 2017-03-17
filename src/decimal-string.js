45c6b94025f60745462b37ca17041c024b3d020f
Alex Reardon <areardon@atlassian.com>
2017-03-17 14:00:59 +1100

    addition and (not working) subtraction

--- src/decimal-string.js
+++ src/decimal-string.js
@@ -1,29 +1,117 @@
 // @flow
 
-// type Options = {|
-//   maxLength: number
-//   |}
-// //options: ?Options = { maxLength: Math.pow(2, 30) }
+type Options = {|
+  maxLength: number
+    |}
 
-// strategy:
-// operate from right to left
-export const add = (value1: string, value2: string): ?string => {
-  const maxSize = Math.max(value1.length, value2.length);
+// Max string sizes
+// ## IE9
+// http://stackoverflow.com/questions/13367391/is-there-a-limit-on-length-of-the-key-string-in-js-object
+// max size: 2^30
+//
+// ## ES7
+// http://www.ecma-international.org/ecma-262/7.0/index.html#sec-ecmascript-language-types-string-type
+// max size: 2^53 - 1
+// this is the same as Number.MAX_SAFE_INTEGER so string.length will always return a valid number
+
+const maxStringLength = Math.pow(2, 30);
+
+const defaultOptions: Options = { maxLength: maxStringLength };
+
+export const add = (term1: string, term2: string, options?: Options = defaultOptions): ?string => {
+  const maxValueLength = Math.max(term1.length, term2.length);
   let digitToCarry = 0;
   let result = '';
 
-  for (let i = maxSize - 1; i > 0; i--) {
-    const digit1 = Number(value1.charAt(i)) || 0;
-    const digit2 = Number(value2.charAt(i)) || 0;
+  for (let i = 0; i < maxValueLength || digitToCarry != 0; i++) {
+    if (result.length + 1 > options.maxLength) {
+      return null;
+    }
+
+    // reading numbers from right to left
+
+    const digit1 = Number(term1.charAt(term1.length - 1 - i)) || 0;
+    const digit2 = Number(term2.charAt(term2.length - 1 - i)) || 0;
 
     const sum = digit1 + digit2 + digitToCarry;
-    const isTooBig = sum > 10;
+    const isTooBig = sum >= 10;
     const resultDigit = isTooBig ? sum - 10 : sum;
     result = `${resultDigit}${result}`;
     digitToCarry = isTooBig ? Number(String(sum).charAt(0)) : 0;
   }
+
+  return result;
 };
 
-export const subtract = (value1: string, value2: string): ?string => {
+export const subtract = (term1: string, term2: string): ?string => {
+  let result = '';
+  const maxValueLength = Math.max(term1.length, term2.length);
 
+  for (let i = 0; i < maxValueLength; i++) {
+    const digit1Index = term1.length - 1 - i;
+    const digit2Index = term2.length - 1 - i;
+
+    // no more digits left in term1
+    if (digit1Index < 0) {
+      const remainingDigitsInTerm2 = term2.substr(0, digit2Index);
+      return `-${remainingDigitsInTerm2}${result}`;
+    }
+
+    const digit1 = Number(term1.charAt(digit1Index)) || 0;
+    const digit2 = Number(term2.charAt(digit2Index)) || 0;
+
+    // easy case
+    const total = digit1 - digit2;
+    if (total >= 0) {
+      const isLastIteration = i == maxValueLength - 1;
+      if (isLastIteration) {
+        return total > 0 ? `${total}${result}` : result;
+      }
+
+      result = `${total}${result}`;
+      continue;
+    }
+
+    // need to borrow numbers from other digits if we can
+    // need to find a previous digit from term1 that has a value > 0
+    // TODO: if there are no more digits left on term1 then we are done..?
+
+    // const remainingDigits = term1.substr(0, term1.length - 1 - i);
+
+    let closestNonZeroIndex = -1;
+    const nextPosition = i + 1;
+    for (let x = nextPosition; x >= 0; x--) {
+      if (term1.charAt(x - 1) !== '0') {
+        closestNonZeroIndex = x - 1;
+        break;
+      }
+    }
+
+    // nothing can be grabbed from term1
+    if (closestNonZeroIndex == -1) {
+      const remainingDigits = term2.substr(0, term2.length - i);
+      return `-${remainingDigits}${result}`;
+    }
+
+    // something can be grabbed from term1
+    term1 = term1.split('')
+      .map((val: string, index: number): string => {
+        // need to decrement the value at term1[closestNonZeroIndex]
+        if (index == closestNonZeroIndex) {
+          return `${Number(val) - 1}`;
+        }
+        // need to set everything between closestNonZeroIndex and i to '9'
+        if (index > closestNonZeroIndex && index < (term1.length - 1 - i)) {
+          return '9';
+        }
+        // unmodified
+        return val;
+      })
+      .join('');
+
+    const resultDigit = (digit1 + 10) - digit2;
+    result = `${resultDigit}${result}`;
+  }
+
+  return result;
 };
--- src/decimal-string.test.js
+++ src/decimal-string.test.js
@@ -1,49 +1,150 @@
 // @flow
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
-import { add } from './decimal-string';
+import { add, subtract } from './decimal-string';
 
 describe('decimal string', () => {
   describe('addition', () => {
     describe('single digits', () => {
-      it('should add two numbers with no overflow', () => {
+      it('should add with no overflow', () => {
         expect(add('1', '2')).to.equal('3');
       });
 
-      it('should add two numbers with overflow', () => {
+      it('should add with overflow', () => {
         expect(add('6', '8')).to.equal('14');
       });
     });
 
+    // TODO: adding positive and negative numbers
+
     describe('two digitis', () => {
-      it('should add two numbers with no overflow', () => {
+      it('should add with one digit numbers', () => {
+        expect(add('10', '2')).to.equal('12');
+      });
+
+      it('should add with one digit numbers with overflow', () => {
+        expect(add('15', '6')).to.equal('21');
+      });
+
+      it('should add with no overflow', () => {
         expect(add('24', '15')).to.equal('39');
       });
 
-      it('should add two numbers with overflow in the 10s digit', () => {
+      it('should add with overflow in the first digit', () => {
         expect(add('15', '16')).to.equal('31');
       });
 
-      it('should add two numbers with overflow in the 100s digit', () => {
+      it('should add with overflow in the second digit', () => {
         expect(add('50', '61')).to.equal('111');
       });
 
-      it('should add two numbers with overflow in the 100s digit', () => {
-        expect(add('50', '61')).to.equal('111');
-      });
-
-      it('should add two numbers with overflow in every digit', () => {
-        expect(add('86', '86')).to.equal('172');
+      it('should add with overflow in every digit', () => {
+        expect(add('99', '99')).to.equal('198');
       });
     });
 
+    describe('three digits', () => {
+      it('should add with overflow in first digit', () => {
+        expect(add('102', '9')).to.equal('111');
+      });
+
+      it('should add with overflow in second digit', () => {
+        expect(add('120', '81')).to.equal('201');
+      });
+
+      it('should add with overflow in the first digit causing overflow in the second digit', () => {
+        expect(add('154', '57')).to.equal('211');
+      });
+
+      it('should add with overflow in the first and second digit', () => {
+        expect(add('199', '99')).to.equal('298');
+      });
+
+      it('should add with overflow in every digit', () => {
+        expect(add('999', '999')).to.equal('1998');
+      });
+    });
+
+    describe.skip('decimals', () => {
+
+    });
+
     describe('it should return null if the overflow is bigger than the max number size', () => {
-      expect(add('50', '51')).to.equal(null);
+      it('should return null when exceeding the maximum allowable length', () => {
+        expect(add('20', '20', { maxLength: 2 })).to.equal('40');
+        expect(add('50', '51', { maxLength: 2 })).to.equal(null);
+      });
+    });
+
+    describe('breaking through Number.MAX_SAFE_INTEGER', () => {
+      it('should be able to deal with numbers greater than Number.MAX_SAFE_INTEGER', () => {
+        // regular numbers fail
+        expect(`${9007199254740991 + 2}`).to.not.equal('9007199254740993');
+
+        // this method succeeds
+        expect(add('9007199254740991', '2')).to.equal('9007199254740993');
+      });
     });
   });
 
   describe('subtraction', () => {
+    describe('single digits', () => {
+      it('should subtract with no overflow', () => {
+        expect(subtract('2', '1')).to.equal('1');
+      });
 
+      it('should subtract going from positive to negative', () => {
+        expect(subtract('2', '3')).to.equal('-1');
+      });
+    });
+
+    describe('double digits', () => {
+      it('should subtract one digit numbers', () => {
+        expect(subtract('16', '5')).to.equal('11');
+      });
+
+      it('should subtract one digit numbers with overflow', () => {
+        expect(subtract('10', '5')).to.equal('5');
+      });
+
+      it('should subtract two digit numbers', () => {
+         expect(subtract('22', '11')).to.equal('11');
+      });
+
+      it('should subtract two digit numbers with overflow in the first digit', () => {
+         expect(subtract('22', '13')).to.equal('9');
+      });
+
+      it('should subtract two digit numbers with overflow in the second digit', () => {
+         expect(subtract('22', '30')).to.equal('-8');
+      });
+
+      it('should subtract two digit numbers with overflow in every digit', () => {
+         expect(subtract('11', '22')).to.equal('-11');
+      });
+    });
+
+    describe('trippled digits', () => {
+      it('should subtract one digit numbers', () => {
+        expect(subtract('101', '1')).to.equal('100');
+      });
+
+      it('should subtract one digit numbers with overflow', () => {
+        expect(subtract('100', '1')).to.equal('99');
+      });
+
+      it('should subtract two digit numbers', () => {
+        expect(subtract('111', '11')).to.equal('100');
+      });
+
+      it.only('should subtract two digit numbers with overflow in the first digit', () => {
+        expect(subtract('121', '12')).to.equal('109');
+      });
+
+      it('should subtract two digit numbers with overflow in every digit', () => {
+        expect(subtract('100', '99')).to.equal('1');
+      });
+    });
   });
 
   describe('multiplication', () => {
